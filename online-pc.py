88723428a3efbf4b525e917c595b04b597783f18
nsingla20 <nsingla20@iitk.ac.in>
2022-08-22 21:58:16 +0530

    added online check

---
+++ online-pc.py
@@ -0,0 +1,47 @@
+#checks online pcs in CSE and CC
+import platform
+import subprocess
+import threading
+
+def ping(server,x,host,ans):
+    """
+    Returns True if host (str) responds to a ping request.
+    Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
+    """
+    loc=server+str(x)+host
+    # Option for the number of packets as a function of
+    param = '-n' if platform.system().lower()=='windows' else '-c'
+
+    # Building the command. Ex: "ping -c 1 google.com"
+    command = ['ping', param, '1','-W 1', loc]
+
+    if subprocess.call(command,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL) == 0:
+        ans.append(x)
+
+def Th100(server,x,host):
+    ans=[]
+    l=[]
+    for i in range(x*100,(x+1)*100):
+        l.append(threading.Thread(target=ping,args=(server,i,host,ans)))
+
+    for t in l:
+        t.start()
+    for t in l:
+        t.join()
+    ans.sort()
+    return ans
+
+def go(x,i,j):  #x=0,cse ; x=1,cc ; from i to j;
+    server="csews" if x==0 else "ccpc"
+    host=".cse.iitk.ac.in" if x==0 else ".cc.iitk.ac.in"
+    print("\nOnline "+("csews" if x==0 else "ccpc")+" pcs are:")
+    ans=[]
+    for e in range(i,j+1):
+        ans=ans+Th100(server,e,host)
+    
+    for e in ans:
+        print(server+str(e)+", ",end="")
+    print("")
+
+go(0,0,3)
+go(1,0,3)
